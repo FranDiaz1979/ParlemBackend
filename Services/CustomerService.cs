7c22db44fdd5479e7df57445993064ea24b303c9
FranDiaz1979 <frandiaz1979@gmail.com>
2023-03-19 14:37:18 +0100

    Add Service layer

---
+++ Services/CustomerService.cs
@@ -0,0 +1,17 @@
+﻿using Models;
+
+namespace Services
+{
+    public class CustomerService
+    {
+        public void Add(Customer customer) 
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer GetById(int id) 
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Services/ProductService.cs
@@ -0,0 +1,22 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class ProductService
+    {
+        public void Add(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Product> GetCustomerList(int customerId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
