849c4467fc537ae696e796a06b0c9c03954571a8
FranDiaz1979 <frandiaz1979@gmail.com>
2023-03-19 14:25:22 +0100

    Add Models layer

---
+++ Models/Customer.cs
@@ -0,0 +1,23 @@
+﻿namespace Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string DocType { get; set; }
+        public string DocNum { get; set; }
+        public string Email { get; set; }
+        public string GivenName { get; set; }
+        public string FamilyName { get; set; }
+        public string Phone { get; set; }
+
+        public Customer()
+        {
+            DocType = string.Empty;
+            DocNum = string.Empty;
+            Email = string.Empty;
+            GivenName = string.Empty;
+            FamilyName = string.Empty;
+            Phone = string.Empty;
+        }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Terminal { get; set; }
+        public DateTime SoldAt { get; set; }
+        public int CustomerId { get; set; }
+
+        public Product()
+        {
+            Name = string.Empty;
+            Type = string.Empty;
+        }
+    }
+}
